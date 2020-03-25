57a2fb05e529bae91706fa71f8b10f211fa2f541
lfoucrit <laurine.foucrit@gmail.com>
2020-03-25 10:28:13 +0100

    Ajout class Building

---
+++ ClassLibrary/Building.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    class Building : IDbEntity
+    {
+        private string name;
+        private int level;
+
+        public int CellNb { get; set; }
+        public List<Resource> TotalCost{ get; set; }
+
+        public List<Resource> NextCost { get; set; }
+    }
+}
